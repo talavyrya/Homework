19258da697f7a69e42e504ad7e6e3497a7ef1548
Dmytro Talavyrya <talavyrya@gmail.com>
2022-07-14 14:11:01 +0300

    task16

--- src/homework/task12/Final.java
+++ src/homework/task12/Final.java
@@ -5,8 +5,14 @@ public class Final {
         Team player1 = new Player1("Robert", 35, "Poland");
         Team player2 = new Player2("Artem", 25, "Ukraine");
         Team one = new Team();
+        Team liverpool = new Team();
         player1.play();
         player2.play();
         one.play();
+        game(player1);
+        game(player2);
+    }
+    public static void game(Team liverpool) {
+        liverpool.play();
     }
 }
